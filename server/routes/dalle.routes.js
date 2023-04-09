5621a8cbc27d4611039ca3359bdd3d134a9a0799
pinky-banik <pinky.rani.banik@gmail.com>
2023-04-09 16:03:58 +0600

    server

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,17 @@
+import express from 'express'; 
+import * as dotenv from 'dotenv';
+import {Configuration,OpenAIApi} from 'openai';
+
+
+dotenv.config();
+
+
+const router = express.Router();
+
+router.route('/').get((req,res)=>{
+    res.status(200).json({message: 'Hello from Dall.E Routes'})
+})
+
+
+export default router;
+
